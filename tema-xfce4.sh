9145fbb003432da2eaffc89f1684db8d5194851c
Joel Barrios <darkshram@users.noreply.github.com>
2022-05-27 19:11:52 -0500

    Update tema-xfce4.sh
    Update copyright year.

--- tema-xfce4.sh
+++ tema-xfce4.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright 2021 Joel Barrios <darkshram@gmail.com>
+# Copyright 2022 Joel Barrios <darkshram@gmail.com>
 
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License as
